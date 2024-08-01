449ef68b29af7564823afce61f712150056c0422
Pooja Garlapati <pgarlapati17@gmail.com>
2024-08-01 00:13:10 -0500

    adding deployment metrics

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    // Ensures that the static export includes trailing slashes if desired
+    trailingSlash: true,
+  
+    // Optional: Specify the output directory for the static export
+    // By default, Next.js exports to the 'out' directory
+    // outDir: 'out',
+  
+    // Optional: Configure basePath if your GitHub Pages site is in a subpath
+    // basePath: '/your-repo-name',
+    
+    // Optional: Configure assetPrefix if needed
+    // assetPrefix: '/your-repo-name/',
+  };
+  
+  export default nextConfig;
