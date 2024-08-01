cce29ad540bf709b104aac8b9a0b48bdb476946a
Pooja Garlapati <pgarlapati17@gmail.com>
2024-08-01 01:00:12 -0500

    updated config

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@
 const nextConfig = {
   output: 'export', // Enables static site export
   trailingSlash: true, // Optional: Adds trailing slashes to routes
-  basePath: '/pantry_tracker', // Set to your GitHub repository name if deploying to a sub-path
-  assetPrefix: '/pantry_tracker/', // Optional: Needed if deploying to a sub-path
+  basePath: '/pantry_tracker', // uncomment if using local --> npm run dev
+  assetPrefix: '/pantry_tracker/', // uncomment if using local -->  npm run dev
 };
 
 export default nextConfig;
