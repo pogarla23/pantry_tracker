fa181170a16779c452c672fb1c00ccc397d076c9
Pooja Garlapati <pgarlapati17@gmail.com>
2024-08-01 00:38:22 -0500

    Update configuration for static export and deploy script

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,10 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Ensures that the static export includes trailing slashes if desired
-    trailingSlash: true,
-  
-    // Optional: Specify the output directory for the static export
-    // By default, Next.js exports to the 'out' directory
-    // outDir: 'out',
-  
-    // Optional: Configure basePath if your GitHub Pages site is in a subpath
-    // basePath: '/your-repo-name',
-    
-    // Optional: Configure assetPrefix if needed
-    // assetPrefix: '/your-repo-name/',
-  };
-  
-  export default nextConfig;
+  output: 'export', // Enables static site export
+  trailingSlash: true, // Optional: Adds trailing slashes to routes
+  basePath: '/pantry_tracker', // Set to your GitHub repository name if deploying to a sub-path
+  assetPrefix: '/pantry_tracker/', // Optional: Needed if deploying to a sub-path
+};
+
+export default nextConfig;
